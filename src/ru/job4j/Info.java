3074778e42954574f50813486be957069daa49f3
AndreyPolegaev <79646307753@yandex.ru>
2021-02-08 12:00:35 +0300

    9.2. Checkstyle [#466158]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -3,9 +3,7 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         int age = 18;
-
         float length = 78.0332F;
-
         boolean baby = true;
         long l = 100;
         System.out.println(baby);
