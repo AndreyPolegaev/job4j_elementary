3f0782b95cd94bce1b5beeec6bc6886d6e218b92
AndreyPolegaev <79646307753@yandex.ru>
2021-02-10 16:22:32 +0300

    4.1.2. Глупый бот [#466085]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -2,19 +2,17 @@ package ru.job4j.condition;
 
 public class DummyBot {
     public static String answer(String question) {
-        String rsl = "";
+    //    Решение цепочкой if - else if
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if (question.equals("Привет Бот")) {
             rsl = "Привет умник";
-        }
-        if (question.equals("Пока.")) {
+        } else if (question.equals("Пока.")) {
             rsl = "До скорой встречи";
-        } else if (!question.equals("Привет Бот") && !question.equals("Пока")) {
-            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
-    }
 
-    public static void main(String[] args) {
+    }
+        public static void main(String[] args) {
         String rsl = DummyBot.answer("Привет Бот");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
