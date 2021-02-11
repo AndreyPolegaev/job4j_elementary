ef64bd49c471795c64629e33173c74f772c62a71
AndreyPolegaev <79646307753@yandex.ru>
2021-02-11 11:06:13 +0300

    4.3.3. Логическое отрицание ! [#466100]

--- src/ru/job4j/condition/LogicNot.java
+++ src/ru/job4j/condition/LogicNot.java
@@ -9,27 +9,29 @@ public class LogicNot {
         return num > 0;
     }
 
-    public boolean notEven(int num) {  // число не четное
-        return num % 2 != 0;
+    public boolean notEven(int num) {  // ' число нечетное
+        return !isEven(num);
     }
 
     public boolean notPositive(int num) { //число не положительное
-        return !(num > 0);
+
+        return !isPositive(num);
     }
 
     public boolean notEvenAndPositive(int num) { //число нечетное и положительное
-        return (num % 2 != 0 && num > 0);
+
+        return (!isEven(num) && isPositive(num));
     }
 
     public boolean evenOrNotPositive(int num) { //число четное или не положительное
-        return (num % 2 == 0 || !(num > 0));
+        return (isEven(num) || !isPositive(num));
     }
 
     public static void main(String[] args) {
         LogicNot lg = new LogicNot();
         System.out.println("4 -  Число четное? " + lg.isEven(4));
         System.out.println("10 - Число положительное? " + lg.isPositive(10));
-        System.out.println("9 -  Число не четное? " + lg.notEven(9));
+        System.out.println("9 -  Число нечетное? " + lg.notEven(9));
         System.out.println("-5 Число не положительное? " + lg.notPositive(-5));
         System.out.println("7 не четное и положительное? " + lg.notEvenAndPositive(7));
         System.out.println("-10 или четное или не положительное " + lg.evenOrNotPositive(-10));
