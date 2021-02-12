a86ee195e4383959e52d1072a0f0fe1cdca9e712
AndreyPolegaev <79646307753@yandex.ru>
2021-02-12 23:44:26 +0300

    5.5. Простое число [#466202]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+// если number простое число (делится на себя и на 1) вернуть true
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
