37d4f421b6abf219a148072135da44547d1aa39d
AndreyPolegaev <79646307753@yandex.ru>
2021-02-12 21:55:03 +0300

    5.4. Протеиновая диета [#466199]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanGreatNik() {
+        int month = Fitness.calc(90, 50);
+        Assert.assertThat(month, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        int month = Fitness.calc(90, 95);
+        assertThat(month, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int month = Fitness.calc(50, 90);
+        assertThat(month, is(2));
+    }
+}
