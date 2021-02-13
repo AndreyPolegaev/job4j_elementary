ed2e5ce8097a15912a68bf11cba9196bf86b4d33
AndreyPolegaev <79646307753@yandex.ru>
2021-02-13 10:13:06 +0300

    5.7. Ипотека [#466201]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+/*
+Метод должен вычислять количество лет, необходимых для погашения кредита.
+использовать цикл while: в теле цикла произвести вычисление кредитной суммы за год плюс остатка.
+amount - сумма выданная по кредиту, salary - годовой доход, percent - процентная ставка по кредиту.
+ */
+public class Mortgage {
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        double ostatok = amount + (amount * percent); // остаток суммы крида + % через год
+        while (ostatok > 0) {
+            double dolg = ostatok - salary;
+            ostatok = dolg + (dolg * percent);
+            year++;
+        }
+        return year;
+    }
+}
