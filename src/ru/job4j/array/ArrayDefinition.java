6a7c7d71fe7f5de27c5dfcc7f207fed6f7d83326
AndreyPolegaev <79646307753@yandex.ru>
2021-02-13 22:48:45 +0300

    6.0.2. Заполнение массива. [#466123]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Andrey";
+        names[1] = "Nikolay";
+        names[2] = "Petr";
+        names[3] = "Stanislav";
         System.out.println("ages length= " + ages.length + " surnames length= " + surnames.length + " prices length= " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
