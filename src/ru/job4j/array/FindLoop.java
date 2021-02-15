b9004c359222889421b2b2d91d89df96b0c6cf8c
AndreyPolegaev <79646307753@yandex.ru>
2021-02-15 20:56:54 +0300

    6.5. Поиск индекса в диапазоне. [#466116]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,5 +1,5 @@
 package ru.job4j.array;
-
+// Поиск индекса элемента в массиве
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
         int rsl = -1;
@@ -10,5 +10,19 @@ public class FindLoop {
             }
         }
         return rsl;
+    }
+    /*
+    data - массив чисел,  el - элемент, который нужно найти, start - индекс, с которого начинаем поиск,
+
+    finish - индекс, которым заканчиваем поиск.
+     */
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rsl = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rsl = i;
+            }
+        }
+        return  rsl;
     }
 }
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -22,4 +22,24 @@ public class FindLoopTest {
         int result = FindLoop.indexOf(arr, 10);
         assertThat(result, is(-1));
     }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenDoesNotFind2() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 5;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        assertThat(result, is(-1));
+    }
 }
