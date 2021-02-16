01b1db6aa2076f440d783fe9620c96ec5b86aa3e
AndreyPolegaev <79646307753@yandex.ru>
2021-02-16 23:00:25 +0300

    6.6.3. Двухмерный массив. If. [#466132]

---
+++ src/ru/job4j/array/MatrixFind.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+// Поиск byltrcf элемента в 2-мерном массиве
+public class MatrixFind {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                if (array[row][cell] == 6) {
+                    System.out.println("Индекс= " + row + " " + cell);
+                }
+            }
+
+        }
+
+    }
+}
